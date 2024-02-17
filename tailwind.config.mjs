cfa0cb41e5435f53c00ba322ee4d66578db25d76
Efrain Garcia <efraingarcia_86@hotmail.com>
2024-02-16 21:18:54 -0700

    Funcionality Light/Dark Mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,13 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-border":
+        "gradient-border-dark":
           "linear-gradient(0deg, rgba(0,8,20,1) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 98%, rgba(0,8,20,1) 100%)",
+        "gradient-border":
+          "linear-gradient(0deg, rgb(255 255 255) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 98%, rgb(255 255 255) 100%)",
+      },
+      colors: {
+        dark: "#000814",
       },
     },
   },
