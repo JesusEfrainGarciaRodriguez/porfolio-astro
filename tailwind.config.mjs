9494a090bb6beb95bbe1c52dc0f1f342eb1b86a2
Efrain Garcia <efraingarcia_86@hotmail.com>
2024-02-16 23:48:39 -0700

    Added pattern

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,8 @@ export default {
         "gradient-border-dark":
           "linear-gradient(0deg, rgba(0,8,20,1) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 98%, rgba(0,8,20,1) 100%)",
         "gradient-border":
-          "linear-gradient(0deg, rgb(255 255 255) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 98%, rgb(255 255 255) 100%)",
+          "linear-gradient(0deg, rgb(255 255 255) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0) 99%, rgb(255 255 255) 100%)",
+        "pattern": "url('./src/assets/so-white.png')",
       },
       colors: {
         dark: "#000814",
