92104e26df9b3f5cf0865a7915735060e1323f1a
Efrain Garcia <efraingarcia_86@hotmail.com>
2024-02-18 20:19:07 -0700

    Fix header light background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,7 @@ export default {
           "linear-gradient(0deg, rgba(0,8,20,1) 0%, rgba(0,0,0,0) 10%, rgba(0,0,0,0) 98%, rgba(0,8,20,1) 100%)",
         "gradient-border":
           "linear-gradient(0deg, rgb(255 255 255) 0%, rgba(0,0,0,0) 1%, rgba(0,0,0,0) 99%, rgb(255 255 255) 100%)",
-        "pattern": "url('src/assets/so-white.png')",
+        "pattern": "url('/src/assets/so-white.png')",
       },
       colors: {
         "dark": "#000814",
